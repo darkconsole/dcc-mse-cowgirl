4bfcfa86897e6c7ee71f50cbbc54b8b4e25d2f0c
darkconsole <darckee.legit@gmail.com>
2016-08-19 22:29:43 -0500

    release script

--- release.bat
+++ release.bat
@@ -1,1 +1,3 @@
-zip dcc-mse-cowgirl-v%~1.zip -r dcc-mse-cowgirl.esp meshes scripts sound textures tools
+zip dcc-mse-cowgirl-v%~1.zip -r dcc-mse-cowgirl.esp meshes scripts sound textures
+zip dcc-mse-cowgirl-bodyslide-v%~1.zip -r tools\CalienteTools\BodySlide\ShapeData tools\CalienteTools\BodySlide\SliderGroups tools\CalienteTools\BodySlide\SliderSets
+zip dcc-mse-bspreset-moo-v%~1.zip -r tools\CalienteTools\BodySlide\SliderPresets
